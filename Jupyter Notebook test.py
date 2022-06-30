864076aa3822ceab64ffb970ae2e58167111bdcd
dhiruvivek <dhiruvivek93@gmail.com>
2022-06-30 12:20:03 +0530

    Add files via upload

---
+++ Jupyter Notebook test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **VIVEK TRIPATHI**
+# My current occupation is video editor and I want to be a Financial Analyst.
+
+# *I am interested in data science because I want to be a Financial Analyst*
+
+# ### This program is a If Else statement
+
+# In[4]:
+
+
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+
+# | Items| price per quantity           | Total Value |
+# | ------------- |:-------------:| -----:|
+# | **Soap**      | 300 | 300 |
+# | **Toothpaste**     | 500     |  1000|
+# | **Chocoltae** |200   |   600|
+# 
+# [this is the link for google](https://www.google.com)
+
+# In[ ]:
+
+
+
+
